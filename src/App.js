1af691fe1b8c6684a97c6ee3d046d80c81da49d4
Dariusz Franczak <darius32@op.pl>
2018-11-15 23:32:26 +0000

    add todo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
       const newTodos = prevState.todos.slice().concat(
         {
           name: todoName,
-          id: uniqid()
+          id: uniqid(),
+          input: ''
         }
       )
       return { todos: newTodos }
@@ -23,12 +24,23 @@ class App extends Component {
   }
 
   //delete todo item method
+  deleteTodo(todoId) {
+    this.setState(prevState => {
+      const newTodos = prevState.todos.slice().filter(el => el.id !== todoId);
+      return { todos: newTodos }
+    })
+
+  }
 
 
   render() {
     return (
       <div className="App">
-
+        <input type="text" value={this.state.input} onChange={e => this.setState({ imput: e.target.value })}></input>
+        <button onClick={this.addTodo.bind(this, this.state.imput)}>Add task</button>
+        <ul>
+          {this.state.todos.map(el => <li key={el.id}>{el.name}</li>)}
+        </ul>
       </div>
     );
   }
